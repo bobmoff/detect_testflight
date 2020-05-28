9d17f25900efabb1e95b89988560d8b0210960c0
Declan Woods <docdecdoc@hotmail.com>
2020-05-28 13:23:01 +0800

    cache value

--- lib/detect_testflight.dart
+++ lib/detect_testflight.dart
@@ -1,17 +1,26 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 import 'dart:io';
 
 class DetectTestflight {
+  static bool _isTestflightCache;
+
   static const MethodChannel _channel = const MethodChannel('detect_testflight');
 
-  static Future<bool> get isTestflight async {
+  static Future<bool> _checkTestflight() async {
     if (Platform.isIOS) {
       final String isTestflight = await _channel.invokeMethod('getPlatformVersion');
       return isTestflight == "true";
     }
 
     return false;
+  }
+
+  static Future<bool> get isTestflight async {
+    if (_isTestflightCache == null) {
+      _isTestflightCache = await _checkTestflight();
+    }
+
+    return _isTestflightCache;
   }
 }
